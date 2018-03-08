72331ecbdf9702185765752d13696819dde9cb6e
Okeke Abuchi <okekeag@gmail.com>
2018-03-08 10:19:03 +0100

    Firmware updated

---
+++ distance_sensor.h
@@ -0,0 +1,29 @@
+/**
+ * @file distance_sensor.h
+ * @brief distance sensor driver definition for the Angelo robot.
+ * @author Miguel Grinberg
+ * Modified by Buchi Okeke
+ */
+
+namespace Angelo
+{
+    class DistanceSensorDriver
+    {
+    public:
+        /**
+          * @brief Class constructor.
+          * @param distance The maximum distance in centimeters that needs to be tracked.
+          */
+        DistanceSensorDriver(unsigned int distance) : maxDistance(distance) {}
+
+        /**
+         * @brief Return the distance to the nearest obstacle in centimeters.
+         * @return the distance to the closest object in centimeters
+         *   or maxDistance if no object was detected
+         */
+        virtual unsigned int getDistance() = 0;
+
+    protected:
+        unsigned int maxDistance;
+    };
+};
